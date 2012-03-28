c53fd8429a0f731e50b592b42f8a99a4d2295a67
sobczak.david <sobczak.david@localhost>
2012-03-28 13:05:23 +0200

    des pages de bases

---
+++ JavaSource/BeanConnexion.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * @author Administrateur
+ *
+ */
+public class BeanConnexion {
+	private boolean isConnected;
+
+	public BeanConnexion() {
+	}
+
+	public boolean getIsConnected() {
+		return isConnected;
+	}
+
+	public void setIsConnected(boolean isConnected) {
+		this.isConnected = isConnected;
+	}
+}
