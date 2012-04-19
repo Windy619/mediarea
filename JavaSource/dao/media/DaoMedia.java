bcebc6cb74a76aaf2ddc76ad5d2968ecf4ac10d0
florence.marchand77 <florence.marchand77@localhost>
2012-04-19 19:12:03 +0200

    lecteur avec liens actualis?s et ressources n?cessaires

--- JavaSource/dao/media/DaoMedia.java
+++ JavaSource/dao/media/DaoMedia.java
@@ -12,6 +12,8 @@ import org.hibernate.criterion.Criterion;
 import org.hibernate.criterion.LogicalExpression;
 import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
+import org.hibernate.Query;
+
 
 import dao.Dao;
 
@@ -93,5 +95,44 @@ public class DaoMedia extends Dao<Media> {
 	public List<?> totalVues(Media media) {
 		return new ArrayList<Media>();
 	}		
+	
+	
+	
+	
+	
+	
+	
+	
+	/**
+	 * Recupération du nombre de commentaires total écrits
+	 * @param idMedia
+	 * @return Un entier
+	 */
+	public int commentairesMedia(int idMedia) {
+		String param = "%" + idMedia + "%";
+		
+		/*Query q = session.createQuery("" +
+				"SELECT COUNT(*) " +
+				"FROM media_commentaire " +
+				"WHERE Media_idMedia = :idMedia");*/
+		
+		int q = ( (Integer) session.createQuery("SELECT COUNT(*) FROM media_commentaire WHERE Media_idMedia = :idMedia").iterate().next() ).intValue();
+		
+		/*q.setParameter("idMedia", param);*/
+
+		//int result=query.UniqueResult<int>();
+		
+		//return q.list().size();
+		//i = list.get(0).intValue();
+		return q;
+		
+		
+		/*ICriteria criteria = Session.CreateCriteria<Post>()
+		    .SetProjection(Projections.Count("Media_idMedia"))
+		    .CreateAlias("media_commentaire", "mc")
+		    .Add(Restrictions.Eq("Media_idMedia", idMedia));
+		 
+		int result = criteria.UniqueResult<int>();*/
+	}
 	
 }
