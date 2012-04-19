d264498dc3dd940b105a0d70cf7158eadc856a13
florence.marchand77 <florence.marchand77@localhost>
2012-04-19 19:12:03 +0200

    lecteur avec liens actualis?s et ressources n?cessaires

---
+++ JavaSource/BeanMedia.java
@@ -0,0 +1,20 @@
+import metier.media.Media;
+
+
+public class BeanMedia {
+
+	private Media mediaVisualise;
+	
+	public BeanMedia() {
+		
+	}
+
+	public Media getMediaVisualise() {
+		return mediaVisualise;
+	}
+
+	public void setMediaVisualise(Media mediaVisualise) {
+		this.mediaVisualise = mediaVisualise;
+	}
+	
+}
