21a29f063f13a5820fcdb3f0c6cc787063dcd4dd
Administrateur <Administrateur@SD>
2012-06-07 22:14:04 +0200

    MAJ RECHERCHE SANS LES RECOMMANDATIONS

---
+++ JavaSource/BeanRecherche.java
@@ -0,0 +1,310 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+
+import dao.media.DaoCategorie;
+import dao.media.DaoMedia;
+import dao.utilisateur.DaoUtilisateur;
+
+import metier.media.Categorie;
+import metier.media.Media;
+import metier.utilisateur.Amitie;
+import metier.utilisateur.Utilisateur;
+
+/**
+ * 
+ */
+
+/**
+ * @author Administrateur
+ *
+ */
+public class BeanRecherche {
+	String value;
+	
+	String typeMedia;
+	String typeRecherche;
+	
+	String tri;
+	String tri_ordre;
+	String page;
+	String nbPages;
+	ArrayList<String> pagination;
+	String nb_par_pages;
+	String afficher_par;
+	Boolean seulementTelechargeable;
+	int nbMediasTrouves;
+	HashMap<Long, Integer> l_mediaVues;
+	HashMap<Long, Integer> l_mediaComs;
+	
+	ArrayList<Media> resRechMedia;
+	DaoMedia daoMedia;
+	
+	
+	String titre;
+	String auteur;
+	String description;
+	String tag;
+	Long categorie;
+	ArrayList<Categorie> lstCategories;
+	DaoCategorie daoCategorie;
+	
+	
+
+	public BeanRecherche() {
+		daoMedia = new DaoMedia();
+		l_mediaVues = new HashMap<Long, Integer>();
+		l_mediaComs = new HashMap<Long, Integer>();
+		resRechMedia = new ArrayList<Media>();
+		typeRecherche = "1";
+		nbMediasTrouves = 0;
+		page = "1";
+		nbPages = "1";
+		pagination = new ArrayList<String>();
+		pagination.add("1");
+		daoCategorie = new DaoCategorie();
+		lstCategories = new ArrayList<Categorie>(daoCategorie.getTous());
+	}
+	
+	public String rechercher(){
+		typeRecherche = "1";
+		titre = null;
+		auteur = null;
+		description = null;
+		tag = null;
+		categorie = null;
+		page = "1";
+		return "/pages/recherche?faces-redirect=true&amp;includeViewParams=true";
+	}
+	
+	public String rechercherAvancee(){
+		typeRecherche = "2";
+		value = null;
+		page = "1";
+		return "/pages/recherche?faces-redirect=true&amp;includeViewParams=true";
+	}
+	
+	public String processFiltre(){
+		page = "1";
+		return "/pages/recherche?faces-redirect=true&amp;includeViewParams=true";
+	}
+	
+	public void processRecherchePre() {
+		if(typeRecherche != null && !typeRecherche.equals("2"))
+			typeRecherche = "1";
+		processRecherche();
+	}
+	
+	public String processRecherche() {
+		HashMap<String, Object> resultat = daoMedia.recherche(value, typeMedia, seulementTelechargeable, tri, tri_ordre, page, nb_par_pages, titre, description, auteur, tag, categorie, typeRecherche);
+		nbMediasTrouves = Integer.parseInt(resultat.get("nbMedias").toString());
+		if(nbMediasTrouves > 0) {
+			l_mediaVues = (HashMap<Long, Integer>)(resultat.get("l_nbVues"));
+			l_mediaComs = (HashMap<Long, Integer>)(resultat.get("l_nbComs"));
+			resRechMedia = new ArrayList((List<Media>)(resultat.get("l_medias")));
+		}
+		else {
+			l_mediaVues.clear();
+			l_mediaComs.clear();
+			resRechMedia.clear();
+		}
+		majPagination();
+		return "/pages/recherche?faces-redirect=true&amp;includeViewParams=true";
+	}
+	
+	public void majPagination() {
+		pagination.clear();
+		int pageActuelle = Integer.parseInt(page);
+		int nbParPage = Integer.parseInt(nb_par_pages);
+		int nbPage = nbMediasTrouves/nbParPage;
+		if(nbMediasTrouves%nbParPage > 0)
+			nbPage++;
+		if(nbPage <= 0)
+			nbPage = 1;
+		nbPages = Integer.toString(nbPage);
+		int nbPageMax = 10;
+		if(nbPage <= nbPageMax) {
+			for(int i = 1; i <= nbPage; i++)
+				pagination.add(Integer.toString(i));
+		}
+		else if (pageActuelle <= (nbPageMax/2)) {
+			for(int i = 1; i <= nbPageMax; i++)
+				pagination.add(Integer.toString(i));		
+		}
+		else {
+			for(int i = (nbPage-(nbPageMax/2)-1); i <= (nbPage+((nbPageMax/2))); i++)
+				pagination.add(Integer.toString(i));
+		}	
+	}
+	
+	public String changePage(String p) {
+		//FacesContext context = FacesContext.getCurrentInstance();  
+		page = p;
+		return "/pages/recherche?faces-redirect=true&amp;includeViewParams=true";
+	}
+	
+	public Boolean getSeulementTelechargeable() {
+		return seulementTelechargeable;
+	}
+	public void setSeulementTelechargeable(Boolean seulementTelechargeable) {
+		this.seulementTelechargeable = seulementTelechargeable;
+	}
+	public String getAfficher_par() {
+		return afficher_par;
+	}
+	public void setAfficher_par(String afficher_par) {
+		this.afficher_par = afficher_par;
+	}
+	public String getTri() {
+		return tri;
+	}
+	public void setTri(String tri) {
+		this.tri = tri;
+	}
+	public String getTri_ordre() {
+		return tri_ordre;
+	}
+	public void setTri_ordre(String tri_ordre) {
+		this.tri_ordre = tri_ordre;
+	}
+	public String getNb_par_pages() {
+		return nb_par_pages;
+	}
+	public void setNb_par_pages(String nb_par_pages) {
+		this.nb_par_pages = nb_par_pages;
+	}
+	public DaoMedia getDaoMedia() {
+		return daoMedia;
+	}
+	public void setDaoMedia(DaoMedia daoMedia) {
+		this.daoMedia = daoMedia;
+	}
+	public ArrayList<Media> getResRechMedia() {
+		return resRechMedia;
+	}
+	public void setResRechMedia(ArrayList<Media> resRechMedia) {
+		this.resRechMedia = resRechMedia;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	public String getTypeMedia() {
+		return typeMedia;
+	}
+	public void setTypeMedia(String typeMedia) {
+		this.typeMedia = typeMedia;
+	}
+	public String getTypeRecherche() {
+		return typeRecherche;
+	}
+	public void setTypeRecherche(String typeRecherche) {
+		this.typeRecherche = typeRecherche;
+	}
+
+	public HashMap<Long, Integer> getL_mediaVues() {
+		return l_mediaVues;
+	}
+
+	public void setL_mediaVues(HashMap<Long, Integer> l_mediaVues) {
+		this.l_mediaVues = l_mediaVues;
+	}
+
+	public HashMap<Long, Integer> getL_mediaComs() {
+		return l_mediaComs;
+	}
+
+	public void setL_mediaComs(HashMap<Long, Integer> l_mediaComs) {
+		this.l_mediaComs = l_mediaComs;
+	}
+
+	public String getPage() {
+		return page;
+	}
+
+	public void setPage(String page) {
+		this.page = page;
+	}
+
+	public int getNbMediasTrouves() {
+		return nbMediasTrouves;
+	}
+
+	public void setNbMediasTrouves(int nbMediasTrouves) {
+		this.nbMediasTrouves = nbMediasTrouves;
+	}
+
+	public String getNbPages() {
+		return nbPages;
+	}
+
+	public void setNbPages(String nbPages) {
+		this.nbPages = nbPages;
+	}
+
+	public ArrayList<String> getPagination() {
+		return pagination;
+	}
+
+	public void setPagination(ArrayList<String> pagination) {
+		this.pagination = pagination;
+	}
+
+	public Long getCategorie() {
+		return categorie;
+	}
+
+	public void setCategorie(Long categorie) {
+		this.categorie = categorie;
+	}
+
+	public ArrayList<Categorie> getLstCategories() {
+		return lstCategories;
+	}
+
+	public void setLstCategories(ArrayList<Categorie> lstCategories) {
+		this.lstCategories = lstCategories;
+	}
+
+	public String getTitre() {
+		return titre;
+	}
+
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+
+	public String getAuteur() {
+		return auteur;
+	}
+
+	public void setAuteur(String auteur) {
+		this.auteur = auteur;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getTag() {
+		return tag;
+	}
+
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+	
+	
+	
+	
+	
+}
--- JavaSource/dao/media/DaoMedia.java
+++ JavaSource/dao/media/DaoMedia.java
@@ -1,6 +1,8 @@
 package dao.media;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 
 import metier.media.Categorie;
@@ -29,7 +31,7 @@ public class DaoMedia extends Dao<Media> {
 	
 	/**
 	 * Recherche avancée
-	 * @param titre
+	 * @param value
 	 * @param description
 	 * @param estTelechargementAutorise
 	 * @param type
@@ -37,8 +39,18 @@ public class DaoMedia extends Dao<Media> {
 	 * @param categorie
 	 * @return Une liste de média
 	 */
-	public List<?> rechercheAvance(String titre, String description, boolean estTelechargementAutorise, Type_Media type, String tag, Categorie categorie) {
-		return new ArrayList<Media>();
+	public List<?> rechercheAvance(String value, String titre, String description, boolean estTelechargementAutorise, Type_Media type, String tag, Long categorie, String typeRecherche) {
+		// A FAIRE
+		Query q = session.createQuery("" +
+				"FROM Media as m " +
+				"WHERE (a.ami.idUtilisateur IN (SELECT a.ami.idUtilisateur FROM Amitie as a WHERE a.utilisateur.idUtilisateur = :idConnecte) " +
+				"	OR a.utilisateur.idUtilisateur IN (SELECT a.ami.idUtilisateur FROM Amitie as a WHERE a.utilisateur.idUtilisateur = :idConnecte) " +
+				"	OR a.utilisateur.idUtilisateur NOT IN (SELECT a.ami FROM Amitie as a WHERE a.utilisateur.idUtilisateur = :idConnecte)) " +
+				"	AND a.utilisateur.idUtilisateur <> :idConnecte " +
+				"");
+		q.setParameter("idConnecte", "bla");	
+		
+		return q.list();
 	}
 	
 	/**
@@ -46,9 +58,9 @@ public class DaoMedia extends Dao<Media> {
 	 * @param mot
 	 * @return Une liste de média
 	 */
-	public List<?> recherche(String mot) {
+	public HashMap<String, Object> recherche(String mot, String typeMedia, Boolean seulementTelechargeable, String tri, String tri_ordre, String page, String nb_par_page, String titre, String description, String auteur, String tag, Long categorie, String typeRecherche) {
 		//return session.getNamedQuery(Media.NQ_RECHERCHE).setParameter("recherche", mot).list();
-		
+		/*
         Criteria crit = session.createCriteria(Media.class);
 
         Criterion titre = Restrictions.like("titreMedia","%".concat(mot).concat("%"));
@@ -62,7 +74,184 @@ public class DaoMedia extends Dao<Media> {
         crit.addOrder(Order.asc("titreMedia")); // On trie correctement
         
 
-        return crit.list(); // On execute la requete		
+        return crit.list(); // On execute la requete
+        //*/	
+		/*
+		Query q = session.createQuery("" +
+				"FROM Media as m " +
+				"WHERE m.titreMedia LIKE :rech_value" +
+				"");
+		q.setParameter("rech_value", mot);
+		//*/
+		///*
+		//*/
+		//*
+		String clauseWhere = "";
+		System.out.println("Creation where");
+		if(typeRecherche != null && typeRecherche.equals("2")) {
+			System.out.println("Avancee");
+			if(titre == null)
+				titre = "";
+			else
+				titre = titre.toUpperCase();
+			clauseWhere += " WHERE (UPPER(m.titreMedia) LIKE '%"+titre.toUpperCase()+"%' ";
+			if(description != null && !description.equals("")) 
+				clauseWhere += "AND UPPER(m.descriptionMedia) LIKE '%"+description.toUpperCase()+"%' ";
+			if(auteur != null && !auteur.equals("")) 
+				clauseWhere += "AND UPPER(ut.pseudo) LIKE '%"+auteur.toUpperCase()+"%' ";
+			if(tag != null && !tag.equals("")) 
+				clauseWhere += "AND UPPER(ta.nomTag) LIKE '%"+tag+"%' ";
+			if(categorie != null && categorie > 0)
+				clauseWhere += "AND mc.categories_idCategorie = " + categorie;
+			clauseWhere += " )";
+			System.out.println(clauseWhere);
+			/*
+			clauseWhere +=  " WHERE (UPPER(m.titreMedia) LIKE '%"+titre+"%'" 
+					+ " AND UPPER(m.descriptionMedia) LIKE '%"+description+"%' "
+					+ " AND UPPER(ut.pseudo) LIKE '%"+auteur+"%' "
+					+ " AND UPPER(ta.nomTag) LIKE '%"+tag+"%' ";
+			System.out.print(clauseWhere);
+			*/
+		}
+		else {
+			if(mot == null) 
+				mot = "";
+			else 
+				mot = mot.toUpperCase();
+			clauseWhere +=  " WHERE (UPPER(m.titreMedia) LIKE '%"+mot+"%'" 
+							+ " OR UPPER(m.descriptionMedia) LIKE '%"+mot+"%') ";
+			
+			titre = mot;
+			description = mot;
+		}
+		if(seulementTelechargeable != null && seulementTelechargeable == true)
+			clauseWhere += "AND m.estTelechargementAutorise = '1' ";
+		if(typeMedia != null && (typeMedia.equals("2") || typeMedia.equals("3"))) {
+			String typeM = "son";
+			if(typeMedia.equals("2"))
+				typeM = "video";
+			clauseWhere += " AND t.nomTypeMedia = '"+ typeM +"'";
+		}
+		
+		HashMap<String, Object> resultat = new HashMap<String, Object>();
+		
+		int nbMedias = 0;
+		HashMap<Long, Integer> l_nbComms = new HashMap<Long, Integer>();
+		HashMap<Long, Integer> l_nbVues = new HashMap<Long, Integer>();
+		
+		Query query_count = session.createSQLQuery(
+				"SELECT COUNT(DISTINCT m.idMedia) AS NB_MEDIA"
+				+ " FROM Media m "
+				+ " LEFT JOIN  utilisateur ut ON m.auteurMedia_idUtilisateur = ut.idUtilisateur "
+				+ " LEFT JOIN  media_categorie mc ON m.idmedia =  mc.media_idmedia "
+				+ " LEFT JOIN  media_tag mt ON m.idmedia = mt.media_idmedia  "
+				+ " LEFT JOIN  tag ta ON mt.tags_idTag = ta.idTag"
+				+ " LEFT JOIN  note n ON m.idmedia = n.media_idmedia "
+				+ " LEFT JOIN  regarder r ON m.idmedia = r.media_idmedia "
+				+ " LEFT JOIN  media_commentaire c ON m.idmedia = c.media_idmedia "
+				+ " LEFT JOIN  type_media t ON m.type_idTypeMedia = t.idTypeMedia "
+				+ clauseWhere
+				);
+		List result_count = query_count.list();
+		Iterator it_count= result_count.iterator();
+		Object o_count = (Object) it_count.next();
+		nbMedias = Integer.parseInt(o_count.toString());
+		
+		String typeOrder = " DESC";
+		if(tri_ordre != null && tri_ordre.equals("1"))
+			typeOrder += " ASC";
+				
+		String clauseOrder = " ORDER BY ";
+		String orderChamps = "NB_TITRE"+typeOrder+", NB_DESCRIPTION"+typeOrder+", VUES_NB"+typeOrder+", m.datePublication"+typeOrder+", NOTE_AVG"+typeOrder+"";
+		if(tri != null && !tri.equals("1")) {
+			if(tri.equals("2"))
+				orderChamps = "VUES_NB"+typeOrder+", NB_TITRE"+typeOrder+", NB_DESCRIPTION"+typeOrder+", m.datePublication"+typeOrder+", NOTE_AVG"+typeOrder+"";
+			else if(tri.equals("3"))
+				orderChamps = "m.datePublication"+typeOrder+", NB_TITRE"+typeOrder+", NB_DESCRIPTION, VUES_NB, NOTE_AVG";
+			else if(tri.equals("4"))
+				orderChamps = "NOTE_AVG"+typeOrder+", NB_TITRE"+typeOrder+", NB_DESCRIPTION"+typeOrder+", VUES_NB "+typeOrder+", m.datePublication"+typeOrder+"";
+		}
+		clauseOrder += orderChamps;
+		
+		
+		int pageI = 1;
+		int nbParPageI = 15;
+		if(page != null && !page.equals("")) {
+			try {
+				pageI = Integer.parseInt(page);
+			}
+			catch(Exception e) {}
+		}
+		if(nb_par_page != null && !nb_par_page.equals("")) {
+			try {
+				nbParPageI = Integer.parseInt(nb_par_page);
+			}
+			catch(Exception e) {}
+		}
+		int depart = (pageI - 1) * nbParPageI;
+		String clauseLimit = " LIMIT " + depart + ", " + nbParPageI;
+	
+		
+		Query query = session.createSQLQuery(
+		"SELECT m.idmedia, COUNT(mt.media_idmedia) AS TAG_NB, COUNT(mc.media_idmedia) AS CAT_NB, AVG(n.note) AS NOTE_AVG, SUM(n.note) AS NOTE_NB, SUM(r.nbVues) AS VUES_NB, COUNT(c.media_idmedia) AS COMM_NB, (LENGTH(m.titreMedia) - LENGTH(REPLACE(UPPER(m.titreMedia), '"+titre+"', '')) + 1) AS NB_TITRE, (LENGTH(m.titreMedia) - LENGTH(REPLACE(UPPER(m.descriptionMedia), '"+description+"', '')) + 1) AS NB_DESCRIPTION "
+		+ " FROM Media m "
+		+ " LEFT JOIN  utilisateur ut ON m.auteurMedia_idUtilisateur = ut.idUtilisateur "
+		+ " LEFT JOIN  media_categorie mc ON m.idmedia =  mc.media_idmedia "
+		+ " LEFT JOIN  media_tag mt ON m.idmedia = mt.media_idmedia  "
+		+ " LEFT JOIN  tag ta ON mt.tags_idTag = ta.idTag"
+		+ " LEFT JOIN  note n ON m.idmedia = n.media_idmedia "
+		+ " LEFT JOIN  regarder r ON m.idmedia = r.media_idmedia "
+		+ " LEFT JOIN  media_commentaire c ON m.idmedia = c.media_idmedia "
+		+ " LEFT JOIN  type_media t ON m.type_idTypeMedia = t.idTypeMedia "
+		+ clauseWhere
+		+ " GROUP BY m.idmedia" +
+		clauseOrder + clauseLimit);
+		//*/
+		/*
+		Query query = session.createSQLQuery(
+				"SELECT * "
+				+ " FROM Media m ").addEntity(Media.class);
+		//*/
+		List result = query.list();
+		String listids = "";
+		Iterator it= result.iterator();
+		while (it.hasNext()) // tant que j'ai un element non parcouru
+		{
+		  Object[] o = (Object[]) it.next();
+		  if(!listids.equals(""))
+			 listids += ", ";
+		  listids += o[0].toString();
+		  
+		  if(o[5] != null && !o[5].toString().equals(""))
+			  l_nbVues.put(Long.parseLong(o[0].toString()), Integer.parseInt(o[5].toString()));
+		  else
+			  l_nbVues.put(Long.parseLong(o[0].toString()), 0);
+		  
+		  if(o[6] != null && !o[6].toString().equals(""))
+			  l_nbComms.put(Long.parseLong(o[0].toString()), Integer.parseInt(o[6].toString()));
+		  else
+			  l_nbComms.put(Long.parseLong(o[0].toString()), 0);
+			  
+		  //mes opérations
+		}
+		if(nbMedias > 0 && listids != null && !listids.equals("")) {
+			Query q = session.createQuery("" +
+					" FROM Media as m " +
+					"WHERE m.idMedia IN (" + listids +")"
+					+ " ORDER BY FIELD(m.idMedia, " + listids + ")");
+			
+			resultat.put("nbMedias", nbMedias);
+			resultat.put("l_nbComs", l_nbComms);
+			resultat.put("l_nbVues", l_nbVues);
+			resultat.put("l_medias", q.list());
+		}
+		else {
+			resultat.put("nbMedias", nbMedias);
+			resultat.put("l_nbComs", null);
+			resultat.put("l_nbVues", null);
+			resultat.put("l_medias", null);			
+		}
+		return resultat;
 	}
 	
 	/**
