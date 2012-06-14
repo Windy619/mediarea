05abdba2bcea13d021cd2f3bd863db43d3512b4a
Florence <Florence@Florence-laptop>
2012-06-14 15:47:44 +0200

    include nvl page commentaires + avis (j'aime, je n'aime pas) fini + favori fini + ajout au panier ok + bon media dans beanPanier + ...

---
+++ JavaSource/dao/media/DaoAimer.java
@@ -0,0 +1,14 @@
+package dao.media;
+
+import metier.media.Aimer;
+import dao.Dao;
+
+public class DaoAimer extends Dao<Aimer>{
+
+	/**
+	 * Constructeur
+	 */
+	public DaoAimer() {
+		super(Aimer.class);
+	}
+}
---
+++ JavaSource/metier/media/Telechargement_Media.java
@@ -0,0 +1,65 @@
+package metier.media;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Telechargement_Media {
+
+	@Id
+	@GeneratedValue
+	private long idTelechargementMedia;
+	
+	@ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+	private Media media;
+
+	private int nbTelechargement;
+	
+	/**
+	 * Constructeur vide
+	 */
+	public Telechargement_Media() {
+		nbTelechargement = 1;
+	}
+	
+	/**
+	 * Constructeur par défaut
+	 * @param nomTag
+	 */
+	public Telechargement_Media(Media media) {
+		this.media = media;
+		nbTelechargement++;
+	}
+	
+	// GETTER SETTER
+	
+
+	public Media getMedia() {
+		return media;
+	}
+
+	public void setMedia(Media media) {
+		this.media = media;
+	}
+
+	public int getNbTelechargement() {
+		return nbTelechargement;
+	}
+
+	public void setNbTelechargement(int nbTelechargement) {
+		this.nbTelechargement = nbTelechargement;
+	}
+
+	public long getIdTelechargementMedia() {
+		return idTelechargementMedia;
+	}
+
+	public void setIdTelechargementMedia(long idTelechargementMedia) {
+		this.idTelechargementMedia = idTelechargementMedia;
+	}	
+	
+	
+}
